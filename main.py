7291828c1e3d4642767048ea40ada86320541c36
REALNAM3 <marckgamer1357@gmail.com>
2025-06-19 16:08:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,16 +28,17 @@ PRESENCE_TYPES = {
 }
 
 ALL_MODS = {
-    "Chase": [22808138, 4782733628, 7447190808],
-    "Orion": [547598710, 5728889572, 4652232128, 7043591647, 5728889572, 4149966999, 7209929547],
+    "Chase": [22808138, 4782733628, 7447190808, 3196162848],
+    "Orion": [547598710, 5728889572, 4652232128, 7043591647, 4149966999, 7209929547, 7043958628, 7418525152, ],
     "LisNix": [162442297, 702354331],
-    "Nwr": [307212658, 5097000699, 4923561416, 4531785383],
+    "Nwr": [307212658, 5097000699, 4923561416],
     "Gorilla": [514679433, 2431747703, 4531785383],
     "Typhon": [2428373515],
     "Vic": [839818760],
     "Erin": [2465133159],
-    "Unknown": [7547477786, 7574577126, 2043525911, 5816563976, 240526951, 4531785383,
-                1160595313, 7876617827, 7693766866, 2568824396, 7604102307, 7587479685],
+    "Ghost": [7558211130],
+    "Unknown": [7547477786, 7574577126, 2043525911, 5816563976, 240526951, 7587479685,
+                1160595313, 7876617827, 7693766866, 2568824396, 7604102307],
 }
 
 class MyClient(discord.Client):
@@ -71,16 +72,17 @@ class MyClient(discord.Client):
                 presence_code = presence_dict.get(uid, 0)
                 user_info = requests.get(f"https://users.roblox.com/v1/users/{uid}")
                 username = user_info.json().get("name", "Unknown") if user_info.status_code == 200 else "Unknown"
+                displayname = user_info.json().get("displayName", "Unknown") if user_info.status_code == 200 else "Unknown"
 
                 if presence_code == 1:
-                    line = f"```ini\n[Online]: {username}\n```"
+                    line = f"```ini\n[Online]: {username} ({displayname})\n```"
                 elif presence_code == 2:
-                    line = f"```diff\n+ In Game: {username}\n```"
+                    line = f"```diff\n+ In Game: {username} ({displayname})\n```"
                     any_in_game = True
                 elif presence_code == 3:
-                    line = f"```fix\nIn Studio: {username}\n```"
+                    line = f"```fix\nIn Studio: {username} ({displayname})\n```"
                 else:
-                    line = f"```diff\n- Offline: {username}\n```"
+                    line = f"```diff\n- Offline: {username} ({displayname})\n```"
                 message_lines.append(line)
             message_lines.append("")
 
